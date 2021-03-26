e6e647d5dec1c756f826a044e09f66f13809adc3
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-03-26 17:31:06 -0300

    Estrategia de Autenticação

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,5 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+// Estrategia de autenticação: 
+//  JWT(Strorage)
+// Next Auth (sistema de autenticação simples, (social), consegue sem o backend)
+// Cognito(AWS), Auth0
+
 export default (request: NextApiRequest, response: NextApiResponse) => {
     const users = [
         {id: 1, name: 'Joao'},
@@ -11,4 +16,3 @@ export default (request: NextApiRequest, response: NextApiResponse) => {
 } 
 
 // Todas são usadas utilizando o Serverless
-// 
