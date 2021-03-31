86ee2481a83d59dd33f8473a3a89aed9df749a63
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-03-31 11:44:30 -0300

    Mostrando o subscriptionId e customerId

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,8 @@
+export async function saveSubscription(
+    subscriptionId: string,
+    customerId: string,
+) {
+    // Buscar o usuario no FAUNADB com o ID customerId
+    // Salavar os dados da subscription no FaunaDB
+    console.log(subscriptionId, customerId);
+}
