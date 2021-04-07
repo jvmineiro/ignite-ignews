4af2f3c892d73e037f11a7ace15417aef1efb677
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-04-07 14:02:48 -0300

    Finished Cap ✅✨🚀

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useSession } from "next-auth/client";
 import  Head  from "next/head";
 import Link from "next/link";
@@ -56,10 +56,12 @@ export default function PostPreview({ post }: PostPreviewProps)  {
     );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async() => {
     return {
         paths: [],
         fallback: 'blocking'
+
+        //fallback: 'true', 'false', 'blocking'
     }
 }
 
@@ -86,6 +88,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return {
         props: {
             post,
-        }
+        },
+        redirect: 60 * 30, // 30 minutes
     }
 } 
